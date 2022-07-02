1bd84d908b3747687d52aff997f439967b258f92
buncho315 <mzh.hrtk125@gmail.com>
2022-07-02 02:32:07 +0000

    Initial Commit3

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   devise_for :users
-  get "home/about"=>"homes#about"
+  get "home/about" => 'homes#about', as: 'about'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'users/:id' => 'users#update', as: 'update_user'
 
-  resources :books, only: [:index, :show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index, :show,:edit,:update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  resources :users, only: [:show,:edit,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
