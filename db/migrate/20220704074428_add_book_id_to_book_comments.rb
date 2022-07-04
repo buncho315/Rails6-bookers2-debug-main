4d7a4f7fb6f681ccd12c5e929fdd9db3ccb4eece
buncho315 <mzh.hrtk125@gmail.com>
2022-07-04 08:26:36 +0000

    Initial Commit9

---
+++ db/migrate/20220704074428_add_book_id_to_book_comments.rb
@@ -0,0 +1,6 @@
+class AddBookIdToBookComments < ActiveRecord::Migration[6.1]
+  def change
+    add_column :book_comments, :book_id, :integer
+    add_column :book_comments, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_052546) do
+ActiveRecord::Schema.define(version: 2022_07_04_074428) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2022_07_04_052546) do
   create_table "book_comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
+    t.integer "user_id"
   end
 
   create_table "books", force: :cascade do |t|
