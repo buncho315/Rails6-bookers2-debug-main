1fc476fa5ee2dc4c4cf0a5a41dba6093299c6a17
buncho315 <mzh.hrtk125@gmail.com>
2022-07-05 09:51:11 +0000

    Initial Commit12

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books
   has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy #自分で追記
+  has_many :book_comments, dependent: :destroy 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
 
   validates :name,length: {minimum: 2, maximum: 20}, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
@@ -6,7 +8,10 @@ Rails.application.routes.draw do
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   patch 'users/:id' => 'users#update', as: 'update_user'
   devise_for :users
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit] #do
+    #resource :relationships, only: [:create, :destroy]
+    #get 'followings' => 'relationships#followings', as: 'followings'
+    #get 'followers' => 'relationships#followers', as: 'followers'
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
