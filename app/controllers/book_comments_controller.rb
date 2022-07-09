e71cc57d9462b532dfd98110c63b0829343cd14e
buncho315 <mzh.hrtk125@gmail.com>
2022-07-09 03:27:38 +0000

    initial C0mmit20

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,13 +3,15 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book.id)
+    unless comment.save
+      render 'error'
+    end
+    #redirect_to book_path(book.id)
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id])
   end
 
   private
