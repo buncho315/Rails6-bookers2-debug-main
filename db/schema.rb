29023a6548780da96d6f2885e7a09460f6a8c115
buncho315 <mzh.hrtk125@gmail.com>
2022-07-10 07:51:32 +0000

    Initial Commit22

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_083616) do
+ActiveRecord::Schema.define(version: 2022_07_10_071439) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_07_05_083616) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "favorites", force: :cascade do |t|
